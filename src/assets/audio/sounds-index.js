f1da6de0d478f53ee55f8e753d784b749be96640
Nick Barnard <barnard.nicholas@googlemail.com>
2022-03-22 22:17:55 +0000

    Move audio to public folder

--- src/assets/audio/sounds-index.js
+++
@@ -1,9 +0,0 @@
-import backgroundData from "./background/background-index";
-import randomData from "./random/random-index";
-
-const audioData = {
-  background: backgroundData,
-  random: randomData
-};
-
-export default audioData;
