a357f19eba583bf12f75fbe9d70d2d7487050fde
Nick Barnard <barnard.nicholas@googlemail.com>
2022-03-23 13:07:32 +0000

    Add isEmpty

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,28 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect } from 'react';
 
 export function usePrevious(value: any) {
-  const ref = useRef<any>(value);
+    const ref = useRef<any>(value);
 
-  useEffect(() => {
-    ref.current = value;
-  });
+    useEffect(() => {
+        ref.current = value;
+    });
 
-  return ref.current;
+    return ref.current;
+}
+
+export function isEmpty(item: any): boolean {
+    if (Array.isArray(item)) return !item.length;
+    if (typeof item === 'string') return !item.trim().length;
+    if (item instanceof Date) return isNaN(item.valueOf());
+    if (typeof item === 'object') return isObjEmpty(item);
+    if (typeof item === 'number') return false;
+
+    return !item;
+}
+
+function isObjEmpty(obj: Record<string, unknown>): boolean {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
 }
