1790c76d76e8ab7269d0ba9868ed3311818dc366
Nick Barnard <barnard.nicholas@googlemail.com>
2022-03-30 14:38:14 +0100

    Add mossy rocks model

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './_styles/App.scss';
 import Scenario from './components/Scenario';
+import ModelTests from './components/ModelTests';
 
 function App() {
     const [showAudio, setShowAudio] = useState(false);
     return (
         <div className="App">
-            {showAudio && <Scenario />}
+            {/* {showAudio && <Scenario />} */}
+            <ModelTests />
             <div className="floating-header">
                 <h1>Three Tests</h1>
                 <button onClick={() => setShowAudio(!showAudio)}>Audio Tests</button>
