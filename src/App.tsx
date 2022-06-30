49be038f413e7009ac5cbdef6e62a483a3b46f46
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-06-30 16:39:02 +0100

    WIP update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
+import './fonts.css'
 import './_styles/App.scss';
 import Scenario from './components/Scenario';
 import { useSelector } from 'react-redux';
 import { getDebug } from './redux/selectors/debug';
 import { useDispatch } from 'react-redux';
 import { toggleDebugState } from './redux/actions/debug';
-import { Action } from 'redux';
+import StyleGuide from './components/StyleGuide';
 
 function App() {
     const [showAudio, setShowAudio] = useState(false);
@@ -14,7 +15,7 @@ function App() {
 
     return (
         <div className="App">
-            {showAudio && <Scenario/>}
+            {/* {showAudio && <Scenario/>}
             <div className="floating-header">
                 <h1>Three Tests</h1>
                 <button onClick={() => setShowAudio(!showAudio)}>Audio Tests</button>
@@ -22,7 +23,8 @@ function App() {
                     //@ts-ignore
                     dispatch(toggleDebugState(!debug))
                 }} >Debug {debug ? 'Off' : 'On'}</button>
-            </div>
+            </div> */}
+            <StyleGuide />
         </div>
     );
 }
--- src/components/CameraRig.tsx
+++ src/components/CameraRig.tsx
@@ -5,7 +5,7 @@ const CameraRig = () => {
     const { camera, mouse } = useThree();
     const vec = new THREE.Vector3();
     return useFrame(() =>
-        camera.position.lerp(vec.set(mouse.x * 2, mouse.y * 1, camera.position.z), 0.02),
+        camera.position.lerp(vec.set(mouse.x * 2, mouse.y * 1, camera.position.z), 0.01),
     );
 };
 
