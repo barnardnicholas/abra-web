605fcb65ca0d297c90425db21c8532dc7cf362b3
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-06-30 17:25:25 +0100

    Add logo & styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { getDebug } from './redux/selectors/debug';
 import { useDispatch } from 'react-redux';
 import { toggleDebugState } from './redux/actions/debug';
 import StyleGuide from './components/StyleGuide';
+import MainLogo from './components/MainLogo';
 
 function App() {
     const [showAudio, setShowAudio] = useState(false);
@@ -14,16 +15,17 @@ function App() {
 
     return (
         <div className="App">
-            {/* {showAudio && <Scenario/>}
+            {showAudio && <Scenario/>}
             <div className="floating-header">
                 <h1>Three Tests</h1>
+                <MainLogo/>
                 <button onClick={() => setShowAudio(!showAudio)}>Audio Tests</button>
                 <button onClick={() => {
                     //@ts-ignore
                     dispatch(toggleDebugState(!debug))
                 }} >Debug {debug ? 'Off' : 'On'}</button>
-            </div> */}
-            <StyleGuide />
+            </div>
+            {/* <StyleGuide /> */}
         </div>
     );
 }
---
+++ src/components/MainLogo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LogoWO from '../assets/logos/ABRA-Logo-Grey-WO.svg'
+
+const MainLogo = () => {
+    return <img src={LogoWO} className="main-logo" width={300} height="auto"/>
+}
+
+export default MainLogo;
