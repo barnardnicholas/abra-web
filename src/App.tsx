27a5f48adcd8d94e4df23ddd9c9bd30d6c095da2
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-01 09:35:53 +0100

    Add animated header prototype

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { useSelector } from 'react-redux';
 import { getDebug } from './redux/selectors/debug';
 import { useDispatch } from 'react-redux';
 import { toggleDebugState } from './redux/actions/debug';
-import StyleGuide from './components/StyleGuide';
 import MainLogo from './components/MainLogo';
 
 function App() {
+    const [compactHeader, setCompactHeader] = useState(false)
     const [showAudio, setShowAudio] = useState(false);
     const dispatch = useDispatch();
     const debug = useSelector(getDebug);
@@ -16,7 +16,7 @@ function App() {
     return (
         <div className="App">
             {showAudio && <Scenario/>}
-            <div className="floating-header">
+            {/* <div className="floating-header">
                 <h1>Three Tests</h1>
                 <MainLogo/>
                 <button onClick={() => setShowAudio(!showAudio)}>Audio Tests</button>
@@ -24,8 +24,13 @@ function App() {
                     //@ts-ignore
                     dispatch(toggleDebugState(!debug))
                 }} >Debug {debug ? 'Off' : 'On'}</button>
-            </div>
+            </div> */}
             {/* <StyleGuide /> */}
+            <header className={compactHeader ? 'compact' : ''}>
+                <div className="item first" onClick={() => setCompactHeader(!compactHeader)}>ITEM 1</div>
+                <div className="item second" onClick={() => setCompactHeader(!compactHeader)}>ITEM 2</div>
+                <div className="item third" onClick={() => setCompactHeader(!compactHeader)}>ITEM 3</div>
+            </header>
         </div>
     );
 }
