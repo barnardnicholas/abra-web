a857284ee21db4a22a7de4c6e29d70155989679c
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-01 11:24:33 +0100

    Rename header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import MainLogo from './MainLogo';
 
-function App() {
-    const [compactHeader, setCompactHeader] = useState(false)
+function Header() {
+    const [compactHeader, setCompactHeader] = useState(false);
 
     return (
-            <header className={compactHeader ? 'compact' : ''}>
-                <div className="header-item logo" onClick={() => setCompactHeader(!compactHeader)}><MainLogo/></div>
-                <div className="header-item title" onClick={() => setCompactHeader(!compactHeader)}><h1>ABRA</h1></div>
-                <div className="header-item chooser" onClick={() => setCompactHeader(!compactHeader)}>Scenario chooser</div>
-            </header>
+        <header className={compactHeader ? 'compact' : ''}>
+            <div className="header-item logo" onClick={() => setCompactHeader(!compactHeader)}>
+                <MainLogo />
+            </div>
+            <div className="header-item title" onClick={() => setCompactHeader(!compactHeader)}>
+                <h1>ABRA</h1>
+            </div>
+            <div className="header-item chooser" onClick={() => setCompactHeader(!compactHeader)}>
+                Scenario chooser
+            </div>
+        </header>
     );
 }
 
-export default App;
+export default Header;
