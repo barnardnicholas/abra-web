4f647e86f07234fb47f67d9bbc44b6f016d0004b
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-01 09:35:38 +0100

    Housekeeping

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoWO from '../assets/logos/ABRA-Logo-Grey-WO.svg'
+import LogoWO from '../assets/logos/ABRA-Logo-Grey-WO.svg';
 
 const MainLogo = () => {
     return <img src={LogoWO} className="main-logo" width={300} height="auto"/>
