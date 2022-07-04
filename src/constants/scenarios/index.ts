d439e08a6044d858dd85b7334be03f5de5e4b253
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-04 19:41:21 +0100

    Add 'none' scenario

--- src/constants/scenarios/index.ts
+++ src/constants/scenarios/index.ts
@@ -1,8 +1,10 @@
 import { Scenario } from '../../types/Scenario';
+import none from './none';
 import rainstorm from './rainstorm';
 import ocean from './ocean';
 
 const scenarios: Record<string, Scenario> = {
+    none,
     rainstorm,
     ocean,
 };
---
+++ src/constants/scenarios/none.ts
@@ -0,0 +1,11 @@
+import { Scenario, soundTypes } from '../../types/Scenario';
+
+const none: Scenario = {
+    id: 1,
+    name: 'None',
+    slug: 'none',
+    description: 'No scenario loaded',
+    sounds: {},
+};
+
+export default none;
