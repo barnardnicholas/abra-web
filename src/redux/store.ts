0796fefba0b5c4a313b54d54353fffa933313ff0
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-06 11:25:22 +0100

    Add scenariosReducer to blacklist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,10 @@
 import { Action } from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 // import { createStore, applyMiddleware } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import rootReducer from './reducers';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
@@ -15,12 +15,13 @@ const middleware = [thunk];
 const persistConfig = {
     key: 'root',
     storage,
-  }
-   
-  const persistedReducer = persistReducer(persistConfig, rootReducer)
+    blacklist: ['scenariosReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
-const store = configureStore({reducer: persistedReducer, middleware});
+const store = configureStore({ reducer: persistedReducer, middleware });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
@@ -31,7 +32,7 @@ export const useAppDispatch = (): Dispatch<Action<any>> => useDispatch<AppDispat
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default () => {
-    let persistor = persistStore(store)
-    return { store, persistor }
-  }
+    let persistor = persistStore(store);
+    return { store, persistor };
+};
 // export default store;
