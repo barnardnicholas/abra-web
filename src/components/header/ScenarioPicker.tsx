d0a333890fd325918cbd747eb33e631ebc6ee763
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-07-22 15:45:18 +0100

    Add more scenarios

--- src/components/header/ScenarioPicker.tsx
+++ src/components/header/ScenarioPicker.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect } from 'react'
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { setScenario } from '../../redux/actions/scenario';
@@ -8,27 +8,38 @@ import { usePrevious } from '../../utils/utils';
 import Select from '../form/Select';
 
 const scenarioOptions: DropdownOption<string>[] = [
-    { label: 'Ocean', value: 'ocean'},
-    { label: 'Rainstorm', value: 'rainstorm'},
-]
+  { label: 'Ocean', value: 'ocean' },
+  { label: 'Rainstorm', value: 'rainstorm' },
+  { label: 'Forest', value: 'forest' },
+  { label: 'City', value: 'city' },
+];
 
 interface ScenarioPickerProps {
-    stopScenario: () => void;
-    isPlaying: boolean;
+  stopScenario: () => void;
+  isPlaying: boolean;
 }
 
-const ScenarioPicker:React.FC<ScenarioPickerProps> = ({stopScenario, isPlaying}) => {
-    const dispatch = useDispatch();
+const ScenarioPicker: React.FC<ScenarioPickerProps> = ({ stopScenario, isPlaying }) => {
+  const dispatch = useDispatch();
 
-    const selectedScenario = useSelector(getSelectedScenario)
+  const selectedScenario = useSelector(getSelectedScenario);
 
-    const handleChange = (_: string, value: string | number | null) => {
-        stopScenario();
-        dispatch(setScenario(null))
-        setTimeout(() =>  dispatch(setScenario(value)), 10);
-    }
+  const handleChange = (_: string, value: string | number | null) => {
+    stopScenario();
+    dispatch(setScenario(null));
+    setTimeout(() => dispatch(setScenario(value)), 10);
+  };
 
-    return <Select name="scenarioPicker" value={selectedScenario} options={scenarioOptions} onChange={handleChange} placeholder="Choose scenario" disabled={isPlaying}/>
-}
+  return (
+    <Select
+      name="scenarioPicker"
+      value={selectedScenario}
+      options={scenarioOptions}
+      onChange={handleChange}
+      placeholder="Choose scenario"
+      disabled={isPlaying}
+    />
+  );
+};
 
 export default ScenarioPicker;
--- src/constants/scenarios/index.ts
+++ src/constants/scenarios/index.ts
@@ -2,10 +2,14 @@ import { Scenario } from '../../types/Scenario';
 import none from './none';
 import rainstorm from './rainstorm';
 import ocean from './ocean';
+import forest from './forest';
+import city from './city';
 
 const scenarios: Record<string, Scenario> = {
-    none,
-    rainstorm,
-    ocean,
+  none,
+  rainstorm,
+  ocean,
+  forest,
+  city,
 };
 export default scenarios;
