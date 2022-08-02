ad5bed008aabb5086c9aaebd4e998164e8c08505
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-02 15:05:37 +0100

    Add camera rotation to rig

--- src/components/scenario/CameraRig.tsx
+++ src/components/scenario/CameraRig.tsx
@@ -3,10 +3,12 @@ import * as THREE from 'three';
 
 const CameraRig = () => {
   const { camera, mouse } = useThree();
+  const origin = new THREE.Vector3();
   const vec = new THREE.Vector3();
-  return useFrame(() =>
-    camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.01),
-  );
+  return useFrame(() => {
+    camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.05);
+    camera.lookAt(origin);
+  });
 };
 
 export default CameraRig;
