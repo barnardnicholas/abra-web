d356b6b095c34553c543b8e7d39369ee69714b1c
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-02 11:43:37 +0100

    Add debug features

--- src/components/debug/Debug.tsx
+++ src/components/debug/Debug.tsx
@@ -1,32 +1,75 @@
-import React from 'react';
+import dayjs from 'dayjs';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { SoundChannel, soundTypes } from '../../types/Scenario';
-import { isEmpty } from '../../utils/utils';
+import { isEmpty, usePrevious } from '../../utils/utils';
 import { UseScenarioProps } from '../hooks/useScenario';
 
-/* eslint-disable */
 interface SoundsTallyItem {
+  fullPath: string;
   fileName: string;
   isPlaying: boolean;
-  duration: string;
+  displayDuration: string;
 }
-/* eslint-enable */
 
 type SoundsTally = Record<string, SoundsTallyItem>;
 
+type LastPlayed = Record<string, number | null>;
+
+function FileDebug({ item, lastPlayed, duration, setLastPlayed }: FileProps) {
+  const { fileName, isPlaying, fullPath, displayDuration } = item;
+  const prevProps = usePrevious({ lastPlayed, isPlaying });
+
+  const playIndicatorStyle = {
+    transition: `max-width ${duration}ms linear`,
+  };
+
+  useEffect(() => {
+    if (!prevProps.isPlaying && isPlaying) {
+      console.log('updating last played');
+      setLastPlayed((prevLastPlayed: LastPlayed) => ({
+        ...prevLastPlayed,
+        [fullPath]: dayjs().valueOf(),
+      }));
+    }
+    /* eslint-disable */
+  }, [isPlaying, prevProps.isPlaying]);
+  /* eslint-enable */
+
+  return (
+    <tr key={fileName}>
+      <td>{fileName}</td>
+      <td>
+        <div className={`play-indicator ${isPlaying ? 'active' : ''}`} style={playIndicatorStyle} />
+        {`${isPlaying ? 'Yes' : ''}`}
+      </td>
+      <td>{displayDuration}</td>
+    </tr>
+  );
+}
+
 function ChannelDebug({ channel }: ChannelProps) {
   const { name, slug, isPlaying, durations, type, paths } = channel;
+
+  const [lastPlayed, setLastPlayed] = useState<LastPlayed>(
+    Object.keys(isPlaying).reduce((acc: LastPlayed, curr: string) => {
+      return { ...acc, [curr]: null };
+    }, {}),
+  );
+
   const soundsTally: SoundsTally = paths.reduce((acc: SoundsTally, curr: string, i: number) => {
     const fileName: string = curr.split('/').pop() || '-';
     const fileSlug: string = fileName.split('.')[0];
     const newItem: SoundsTallyItem = {
       fileName,
+      fullPath: curr,
       isPlaying: isPlaying[curr] || false,
-      duration: !Number.isNaN(durations[i]) ? `${(durations[i] / 1000).toFixed(1)}s` : '0',
+      displayDuration: !Number.isNaN(durations[i]) ? `${(durations[i] / 1000).toFixed(1)}s` : '0',
     };
     return { ...acc, [fileSlug]: newItem };
   }, {} as SoundsTally);
+
   return (
-    <div>
+    <div className="debug-item">
       <strong>{`${soundTypes[type]} Channel: ${name} (${slug})`}</strong>
       <div>
         <table className="debug-table">
@@ -38,17 +81,16 @@ function ChannelDebug({ channel }: ChannelProps) {
             </tr>
           </thead>
           <tbody>
-            {Object.values(soundsTally).map(
-              /* eslint-disable */
-              ({ fileName, isPlaying, duration }: SoundsTallyItem) => (
-                /* eslint-enable */
-                <tr key={fileName}>
-                  <td>{fileName}</td>
-                  <td>{`${isPlaying ? 'Yes' : ''}`}</td>
-                  <td>{duration}</td>
-                </tr>
-              ),
-            )}
+            {Object.values(soundsTally).map((item: SoundsTallyItem, i: number) => {
+              return (
+                <FileDebug
+                  item={item}
+                  lastPlayed={lastPlayed[item.fullPath]}
+                  duration={durations[i]}
+                  setLastPlayed={setLastPlayed}
+                />
+              );
+            })}
           </tbody>
         </table>
       </div>
@@ -64,12 +106,21 @@ function Debug({ scenario }: DebugProps) {
         <div>{`Scenario isPlaying: ${scenario.isPlaying}`}</div>
         <br />
       </div>
-      {!isEmpty(scenario.soundChannels) &&
-        Object.values(scenario.soundChannels).map((channel: SoundChannel) => (
-          <ChannelDebug key={channel.id} channel={channel} />
-        ))}
+      <div className="item-container">
+        {!isEmpty(scenario.soundChannels) &&
+          Object.values(scenario.soundChannels).map((channel: SoundChannel) => (
+            <ChannelDebug key={channel.id} channel={channel} />
+          ))}
+      </div>
     </div>
   );
+}
+
+interface FileProps {
+  item: SoundsTallyItem;
+  lastPlayed: number | null;
+  duration: number;
+  setLastPlayed: Dispatch<SetStateAction<LastPlayed>>;
 }
 
 interface DebugProps {
