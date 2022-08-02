1092d41d5af353bb22667509d2b5ccb1899b8b73
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-02 15:41:55 +0100

    Tweak z coords on camerarig

--- src/components/scenario/CameraRig.tsx
+++ src/components/scenario/CameraRig.tsx
@@ -4,12 +4,12 @@ import { getMouseDistanceFromCenter } from '../../utils/utils';
 
 const CameraRig = () => {
   const { camera, mouse } = useThree();
-  const origin = new THREE.Vector3();
+  const origin = new THREE.Vector3(0, 0.5, 0);
   const vec = new THREE.Vector3();
-  const pZ = camera.position.z;
+  const pZ = 4;
   return useFrame(() => {
     camera.position.lerp(
-      vec.set(mouse.x * -1, mouse.y * -1, pZ + -0.1 * getMouseDistanceFromCenter(mouse.x, mouse.y)),
+      vec.set(mouse.x * -1, mouse.y * -1, pZ + -0.2 * getMouseDistanceFromCenter(mouse.x, mouse.y)),
       0.05,
     );
     camera.lookAt(origin);
