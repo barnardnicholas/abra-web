d070d66178e67695a2513412938e7d41b10174cc
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-05 16:12:19 +0100

    Remove concurrent debug control

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleDarkMode } from '../../redux/actions/darkMode';
-import { toggleConcurrentAudio, toggleDebug } from '../../redux/actions/debug';
+import { toggleDebug } from '../../redux/actions/debug';
 import { getDarkMode } from '../../redux/selectors/darkMode';
-import { getConcurrentAudio, getDebug } from '../../redux/selectors/debug';
+import { getDebug } from '../../redux/selectors/debug';
 import { getShowSettings } from '../../redux/selectors/settings';
 import Divider from '../common/Divider';
 import ToggleSwitch from '../form/ToggleSwitch';
@@ -13,7 +13,6 @@ function Settings() {
   const showSettings = useSelector(getShowSettings);
   const isDebug = useSelector(getDebug);
   const isDarkMode = useSelector(getDarkMode);
-  const concurrentAudio = useSelector(getConcurrentAudio);
   return (
     <div className={`settings-container ${showSettings ? 'expanded' : ''}`}>
       <div className="settings">
@@ -31,13 +30,6 @@ function Settings() {
           value={isDarkMode}
           name="darkMode"
           onChange={() => dispatch(toggleDarkMode(!isDarkMode))}
-        />
-        <Divider />
-        <ToggleSwitch
-          label="Concurrent audio clocks"
-          value={concurrentAudio}
-          name="concorrentAudio"
-          onChange={() => dispatch(toggleConcurrentAudio(!concurrentAudio))}
         />
         <Divider />
       </div>
