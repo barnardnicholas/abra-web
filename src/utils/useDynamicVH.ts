4ed1acdf043592b1f48e9c744690ac91d23e70bf
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-05 16:59:43 +0100

    Add documentation to hooks

--- src/utils/useDynamicVH.ts
+++ src/utils/useDynamicVH.ts
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
+/**
+ * Used for window height bug on mobile devices - sets the CSS variable '--vh' to 1/100 of the window height
+ * @return {void} Nothing
+ */
 const useDynamicVH = () => {
+  function update() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 
-    function update() {
-        const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-    }
-
-    useEffect(() => {
-        update();
-    },[])
-}
+  useEffect(() => {
+    update();
+  }, []);
+};
 
 export default useDynamicVH;
