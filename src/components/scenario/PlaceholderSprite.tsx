332c27f37f20dd844cfadaa70fdc0bea0a0afc74
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-05 17:36:49 +0100

    Make placeholder size change with small screen

--- src/components/scenario/PlaceholderSprite.tsx
+++ src/components/scenario/PlaceholderSprite.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Euler, MeshBasicMaterial, TextureLoader } from 'three';
+import useIsSmallScreen from '../hooks/useIsSmallScreen';
 
 interface PlaceholderProps {
   visible: boolean;
 }
 
 function PlaceholderSprite({ visible }: PlaceholderProps) {
+  const isSmallScreen = useIsSmallScreen();
   const loader = new TextureLoader();
   const texture = loader.load('sprites/placeholder.png');
   const material = new MeshBasicMaterial({ map: texture, transparent: true });
@@ -17,7 +19,7 @@ function PlaceholderSprite({ visible }: PlaceholderProps) {
       material={material}
       visible={visible}
     >
-      <planeGeometry attach="geometry" args={[4, 4]} />
+      <planeGeometry attach="geometry" args={isSmallScreen ? [2, 2] : [4, 4]} />
     </mesh>
   );
 }
